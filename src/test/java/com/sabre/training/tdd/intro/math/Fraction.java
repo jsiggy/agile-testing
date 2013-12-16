071a0e64b58e248192ac2d24862d6e2dd9ea791a
John Sigler <john.sigler@gmail.com>
2013-12-15 18:02:42 -0600

    Show same denominator

--- src/test/java/com/sabre/training/tdd/intro/math/Fraction.java
+++ src/test/java/com/sabre/training/tdd/intro/math/Fraction.java
@@ -1,18 +1,32 @@
 package com.sabre.training.tdd.intro.math;
 
 public class Fraction {
-   private int numerator;
+   private final int numerator;
+   private final int denominator;
 
    public Fraction(int numerator) {
-
       this.numerator = numerator;
+      this.denominator = 1;
+   }
+
+   public Fraction(int numerator, int denominator) {
+      this.numerator = numerator;
+      this.denominator = denominator;
    }
 
    public Fraction plus(Fraction that) {
-      return new Fraction(this.numerator + that.numerator);
+      return new Fraction(this.numerator + that.numerator, this.denominator);
    }
 
    public int intValue() {
       return this.numerator;
+   }
+
+   public int numerator() {
+      return numerator;
+   }
+
+   public int denominator() {
+      return denominator;
    }
 }
--- src/test/java/com/sabre/training/tdd/intro/math/FractionTest.java
+++ src/test/java/com/sabre/training/tdd/intro/math/FractionTest.java
@@ -12,29 +12,28 @@ public class FractionTest {
 
    @Test
    public void zeroPlusNonZero() {
-      Fraction sum = new Fraction(0).plus(new Fraction(3));
-      assertEquals(3, sum.intValue());
+      assertEquals(3, new Fraction(0).plus(new Fraction(3)).intValue());
    }
 
    @Test
    public void nonZeroPlusZero() {
-      Fraction first = new Fraction(5);
-      Fraction second = new Fraction(0);
-
-      Fraction sum = first.plus(second);
-
-      assertEquals(5, sum.intValue());
+      assertEquals(5, new Fraction(5).plus(new Fraction(0)).intValue());
    }
 
    @Test
    public void nonZeroPlusNonZero() {
-      Fraction sum = new Fraction(3).plus(new Fraction(4));
-      assertEquals(7, sum.intValue());
+      assertEquals(7, new Fraction(3).plus(new Fraction(4)).intValue());
    }
 
    @Test
    public void negativePlusNegative() {
-      Fraction sum = new Fraction(-2).plus(new Fraction(-3));
-      assertEquals(-5, sum.intValue());
+      assertEquals(-5, new Fraction(-2).plus(new Fraction(-3)).intValue());
+   }
+
+   @Test
+   public void addWithSameDenominator() {
+      Fraction sum = new Fraction(1, 5).plus(new Fraction(2, 5));
+      assertEquals(3, sum.numerator());
+      assertEquals(5, sum.denominator());
    }
 }
