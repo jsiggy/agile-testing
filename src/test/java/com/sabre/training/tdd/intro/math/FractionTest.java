35e065951a888e3ecec676ab53d22fb8f9aa256d
John Sigler <john.sigler@gmail.com>
2013-12-15 18:49:47 -0600

    Use equals for all the existing tests

--- src/test/java/com/sabre/training/tdd/intro/math/FractionTest.java
+++ src/test/java/com/sabre/training/tdd/intro/math/FractionTest.java
@@ -27,34 +27,31 @@ public class FractionTest {
 
    @Test
    public void zeroPlusZero() {
-      assertEquals(0, new Fraction(0).plus(new Fraction(0)).numerator());
+      assertEquals(new Fraction(0), new Fraction(0).plus(new Fraction(0)));
    }
 
    @Test
    public void zeroPlusNonZero() {
-      assertEquals(3, new Fraction(0).plus(new Fraction(3)).numerator());
+      assertEquals(new Fraction(3), new Fraction(0).plus(new Fraction(3)));
    }
 
    @Test
    public void nonZeroPlusZero() {
-      assertEquals(5, new Fraction(5).plus(new Fraction(0)).numerator());
+      assertEquals(new Fraction(5), new Fraction(5).plus(new Fraction(0)));
    }
 
    @Test
    public void nonZeroPlusNonZero() {
-      assertEquals(7, new Fraction(3).plus(new Fraction(4)).numerator());
+      assertEquals(new Fraction(7), new Fraction(3).plus(new Fraction(4)));
    }
 
    @Test
    public void negativePlusNegative() {
-      Fraction fraction = new Fraction(-2).plus(new Fraction(-3));
-      assertEquals(-5, fraction.numerator());
+      assertEquals(new Fraction(-5), new Fraction(-2).plus(new Fraction(-3)));
    }
 
    @Test
    public void addWithSameDenominator() {
-      Fraction sum = new Fraction(1, 5).plus(new Fraction(2, 5));
-      assertEquals(3, sum.numerator());
-      assertEquals(5, sum.denominator());
+      assertEquals(new Fraction(3, 5), new Fraction(1, 5).plus(new Fraction(2, 5)));
    }
 }
