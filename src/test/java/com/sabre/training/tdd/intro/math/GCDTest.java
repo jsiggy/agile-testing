e037f583c81bc0f28439df341a3708a2d1936b7c
John Sigler <john.sigler@gmail.com>
2013-12-15 21:11:14 -0600

    gcd - fix negatives

--- src/test/java/com/sabre/training/tdd/intro/math/GCDTest.java
+++ src/test/java/com/sabre/training/tdd/intro/math/GCDTest.java
@@ -9,14 +9,14 @@ public class GCDTest {
    public void reflexiveTests() {
       assertEquals(1, gcd(1, 1));
       assertEquals(2, gcd(2, 2));
-      assertEquals(-1, gcd(-1, -1));
+      assertEquals(1, gcd(-1, -1));
    }
 
    @Test
    public void noCommonFactor() {
       assertEquals(1, gcd(4, 7));
       assertEquals(1, gcd(2, 3));
-      assertEquals(-1, gcd(-2, -3));
+      assertEquals(1, gcd(-2, -3));
    }
 
    @Test
@@ -29,7 +29,13 @@ public class GCDTest {
    public void commonFactors() {
       assertEquals(2, gcd(6, 8));
       assertEquals(7, gcd(49, 315));
-      assertEquals(-4, gcd(-24, -28));
+   }
+
+   @Test
+   public void commonFactorNegatives() {
+      assertEquals(4, gcd(-24, -28));
+      assertEquals(4, gcd(24, -28));
+      assertEquals(4, gcd(-24, 28));
    }
 
    private int gcd(int a, int b) {
@@ -38,6 +44,6 @@ public class GCDTest {
          b = a % t;
          a = t;
       }
-      return a;
+      return Math.abs(a);
    }
 }
