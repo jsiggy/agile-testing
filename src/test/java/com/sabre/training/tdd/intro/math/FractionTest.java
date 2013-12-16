73d83485faf7f4995973636bf65195f6375fa26f
John Sigler <john.sigler@gmail.com>
2013-12-15 21:44:04 -0600

    more tests for reducing while adding fractions, found a missing case for gcd

--- src/test/java/com/sabre/training/tdd/intro/math/FractionTest.java
+++ src/test/java/com/sabre/training/tdd/intro/math/FractionTest.java
@@ -1,10 +1,13 @@
 package com.sabre.training.tdd.intro.math;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class FractionTest {
+
+   // equals
    @Test
    public void equalsForSameNumeratorAndDenominator() {
       assertEquals(new Fraction(3, 5), new Fraction(3, 5));
@@ -25,6 +28,7 @@ public class FractionTest {
       assertEquals(new Fraction(5), new Fraction(5, 1));
    }
 
+   // addition
    @Test
    public void zeroPlusZero() {
       assertEquals(new Fraction(0), new Fraction(0).plus(new Fraction(0)));
@@ -60,6 +64,39 @@ public class FractionTest {
       assertEquals(new Fraction(5, 6), new Fraction(1, 2).plus(new Fraction(1, 3)));
    }
 
+   @Test
+   public void reduceResultToWholeNumber() {
+      assertEquals(new Fraction(1), new Fraction(1, 3).plus(new Fraction(2, 3)));
+   }
+
+   @Test
+   public void oneFactorIsAMultipleOfTheOther() {
+      assertEquals(new Fraction(11, 8), new Fraction(3, 4).plus(new Fraction(5, 8)));
+   }
+
+   @Test
+   public void commonFactorsInDenominators() {
+      assertEquals(new Fraction(11, 18), new Fraction(1, 6).plus(new Fraction(4, 9)));
+   }
+
+   @Test
+   public void reduceResultsEvenWhenDenominatorsAreTheSame() {
+      assertEquals(new Fraction(3, 2), new Fraction(3, 4).plus(new Fraction(3, 4)));
+   }
+
+   @Test
+   public void negativeFractionIsReducing() {
+      assertEquals(new Fraction(1, 2), new Fraction(-1, 4).plus(new Fraction(3, 4)));
+      assertEquals(new Fraction(-1, 8), new Fraction(3, 8).plus(new Fraction(-1, 2)));
+   }
+
+   @Test
+   @Ignore("Waiting on special case handling for gcd")
+   public void reduceToNegativeNumeratorAndDenominator() {
+      assertEquals(new Fraction(1, 2), new Fraction(-1, 4).plus(new Fraction(-3, -4)));
+   }
+
+   // reduce
    @Test
    public void reduceWhenAlreadyInLowestTerms() {
       assertEquals(new Fraction(3, 4), new Fraction(3, 4));
